16a6a77383ff922d49861661b91525f80cf86f84
Vedanti00 <vedantisinkar959@gmail.com>
2022-10-23 14:52:11 +0530

    Username Password: through register and login

--- app.js
+++ app.js
@@ -15,6 +15,60 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.post("/login", function(req, res) {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: email}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else {
+        if (foundUser){
+          if(password === password){
+            res.render("secrets");
+          }
+        }
+      }
+  });
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+
+});
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
